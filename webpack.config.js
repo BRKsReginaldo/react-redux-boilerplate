bbbc14c017c9e421f348653dc67f6af398cbb544
Reginaldo Junior <reginaldo@deltacoding.xyz>
2018-04-06 14:02:26 -0300

    updated boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,104 @@
 const webpack = require('webpack')
-const MinifyPlugin = require("babel-minify-webpack-plugin")
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const path = require('path')
 
-let plugins = [
-  new ExtractTextPlugin("assets/css/app.css?v=[chunkhash:6]"),
-  new HtmlWebpackPlugin({
-    template: './template/template.html'
-  }),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-  })
-]
+module.exports = (a, env) => {
+  let devtool = 'inline-source-map'
+  let plugins = [
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/app.css?v=[hash:6]',
+      chunkFilename: 'assets/css/app.css?v=[hash:6]',
+    }),
+    new HtmlWebpackPlugin({
+      template: './template/template.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env.mode || 'development')
+    }),
+    new webpack.NamedModulesPlugin()
+  ]
 
-if (process.env.NODE_ENV === 'production') {
-  plugins.push(new MinifyPlugin())
-  plugins.push(new OptimizeCssAssetsPlugin({
-    assetNameRegExp: /\.css.*$/,
-    cssProcessorOptions: { discardComments: { removeAll: true } }
-  }))
-}
+  if (env.mode === 'production') {
+    plugins.push(new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css.*$/,
+      cssProcessorOptions: {discardComments: {removeAll: true}}
+    }))
+    devtool = 'source-map'
+  }
 
-module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname + '/public',
-    filename: 'assets/js/app.js?v=[chunkhash:6]'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/, 
-        use: ExtractTextPlugin.extract('css-loader')
-      },
-      {
-        test: /\.styl$/, 
-        use: ExtractTextPlugin.extract('css-loader!stylus-loader')
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract('css-loader!sass-loader')
-      }
-    ]
-  },
-  plugins: plugins,
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './public'
+  return {
+    entry: [
+      'babel-polyfill',
+      './src/index.js'
+    ],
+    devtool: devtool,
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'assets/js/app.js?v=[hash:6]',
+      chunkFilename: 'assets/js/[name].chunk.js?v=[hash:6]',
+      publicPath: "/"
+    },
+    resolve: {
+      extensions: [
+        '.js',
+        '.jsx',
+        '.css',
+        '.sass',
+        '.styl'
+      ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader'
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader'
+            }
+          ]
+        },
+        {
+          test: /\.scss/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        },
+        {
+          test: /\.styl$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'stylus-loader'
+            }
+          ]
+        }
+      ]
+    },
+    plugins: plugins,
+    devServer: {
+      contentBase: './public',
+      historyApiFallback: true
+    },
   }
 }
